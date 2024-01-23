b9c1afd06bd3c6eb9f8f80b65c5e40292210456c
Atharva253 <atharvashinde253@gmail.com>
2024-01-24 02:26:25 +0530

    Added Alert to input element

---
+++ script.js
@@ -0,0 +1,13 @@
+const todoValue = document.getElementById("todoText"),
+    listItems = document.getElementById("list-items"),
+    addUpdateClick = document.getElementById("AddUpdateClick");
+
+function CreateToDoData() {
+    if (todoValue.value===""){
+        alert("Please Enter your todo text");
+        todoValue.focus();
+
+    }
+    let li = document.createElement("li");
+    const todoItems = "<div>Hello</div>"
+}
